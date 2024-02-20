789a6b16955433d40a68970dfa1b41662eb3a84a
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-02-20 13:01:26 -0600

    ⭐Feat: Author Name Create the Author name componet

---
+++ src/components/AuthorName.vue
@@ -0,0 +1,8 @@
+<template>
+    <div> 
+        <h1> Author Name: </h1>
+        <p> <a target="_blank" href="https://github.com/JenniferAw26 "> Jennifer Aguado
+        </a>
+    </p>
+    </div>
+</template>
