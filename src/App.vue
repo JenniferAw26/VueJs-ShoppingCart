7f2512327d3f77f2df412489eeedc25e32d43374
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-03-14 10:59:50 -0600

    ⭐ Feat: Creacion de Metodos

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,59 @@
 <script setup>
-
-// Importando funcion para crear referencias reactivas ctrl+k+c
-import{ref} from 'vue'
-// Creando una referencia reactiva de tipo string
-const header = ref(' App lista de compras');
-const shoppingIcon= ref('material-icons shopping-cart-icon');
-// Creando una referencia reactiva para almacenar el valor de la lista
-const items= ref([
-  {id: 0,label:'Leche'},
-  {id: 1,label:'Arroz'},
-  {id: 2,label:'Carne'},
-  {id: 3, label:'Pan'},
-  {id: 4,label:'Huevo'}]);
-
+//Importando una funcion para crear referencias reactivas
+import { ref } from 'vue'
+//Creando una referencia reactiva de tipo string
+const header = ref('App lista de compras');
+const shoppingIcon = ref("material-icons shopping-cart-icon")
+//Creando una referencia reactiva para almacenar el valor de la lista
+const items = ref([
+{id: 0 , label: 'Leche'},
+{id: 1 , label: 'Arroz'},
+{id: 2 , label: 'Carne'},
+{id: 3 , label: 'Pan'},
+{id: 4 , label: 'Huevos'}
+]);
+ 
 const newItem = ref('');
-const newItemHighPriority = ref('false')
+const newItemHighPriority = ref(false)
+ 
+//Metodos, son funciones de javascript
+const saveItems = () => {
+  //Agrega un nuevo elemento a la lista proveniente de la caja de texto
+  items.value.push({ id: items.value.length, label: newItem.value})
+  //Borramos el contenido de la caja de texto
+  newItem.value = "";
+};
 </script>
-
+ 
 <template>
-  <h1><i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
-  <div class="add-item form">
-     <input v-model ="newItem" type="text" placeholder="Agregar Articulo">
-     <!-- Check Box -->
-     <label><input type="checkbox" v-model="newItemHighPriority">Alta Prioridad</label>
-     <!-- Boton -->
-     <button class="btn btn-primary" v-on:click="items.push({id:items.length,label:newItem})">Agregar Articulo</button>
-  </div>
+  <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <form
+    v-on:submit.prevent="saveItems"
+    class="add-item form">
+    <input
+    v-model ="newItem" type="text"
+    placeholder="Agregar Articulo">
+ 
+    <!-- Checkbox -->
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority">
+      Alta Prioridad
+    </label>
+ 
+    <!-- Boton -->
+    <button
+      class="btn btn-primary">
+      Agregar Articulo
+    </button>
+  </form>
+ 
   <ul>
-   <li v-for="({id,label},i) in items" v-bind:key="id">⭐{{ i }} {{label}}</li> 
+    <li v-for="({ id, label }, i) in items" v-bind:key="id">🌟 {{ i }} {{ label }}</li>
   </ul>
 </template>
-
+ 
 <style scoped>
-.shopping-cart-icon{font-size: 2rem;}
+.shopping-cart-icon{
+  font-size: 2rem;
+}
 </style>
