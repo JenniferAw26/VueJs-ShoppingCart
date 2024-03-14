23b7c25d93a3c5ef2239b3756c3519ce20af0024
Jennifer Aguado <jenniferaguadowong@gmail.com>
2024-03-14 12:49:47 -0600

    ⭐ Feat: Evaluacion Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,16 @@ const header = ref('App lista de compras');
 const shoppingIcon = ref("material-icons shopping-cart-icon")
 //Creando una referencia reactiva para almacenar el valor de la lista
 const items = ref([
-{id: 0 , label: 'Leche'},
-{id: 1 , label: 'Arroz'},
-{id: 2 , label: 'Carne'},
-{id: 3 , label: 'Pan'},
-{id: 4 , label: 'Huevos'}
+// {id: 0 , label: 'Leche'},
+// {id: 1 , label: 'Arroz'},
+// {id: 2 , label: 'Carne'},
+// {id: 3 , label: 'Pan'},
+// {id: 4 , label: 'Huevos'}
 ]);
  
 const newItem = ref('');
 const newItemHighPriority = ref(false)
+const showForm = ref(false); // Se crea variable reactiva para controlar la visibilidad de los botones en el formulario
  
 //Metodos, son funciones de javascript
 const saveItems = () => {
@@ -23,16 +24,22 @@ const saveItems = () => {
   //Borramos el contenido de la caja de texto
   newItem.value = "";
 };
+ //Entrega Condicional
+//const noItemsMessage =ref (true);
 </script>
  
 <template>
-  <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
-  <form
+  <!-- Header -->
+  <div class="header">
+    <h1> <i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+    <button v-if="showForm" class="btn" @click="showForm = false">Cancelar ❌</button>
+    <button v-if="!showForm" class="btn" @click="showForm = true">Agregar Articulo ✅</button>
+  </div>
+  <!-- Formulario -->
+  <form v-if="showForm"
     v-on:submit.prevent="saveItems"
     class="add-item form">
-    <input
-    v-model ="newItem" type="text"
-    placeholder="Agregar Articulo">
+    <input v-model="newItem" type="text" placeholder="Agregar Articulo">
  
     <!-- Checkbox -->
     <label>
@@ -46,10 +53,12 @@ const saveItems = () => {
       Agregar Articulo
     </button>
   </form>
- 
+ <!-- Entrega de lista -->
   <ul>
     <li v-for="({ id, label }, i) in items" v-bind:key="id">🌟 {{ i }} {{ label }}</li>
   </ul>
+  <!-- Mensaje condicional -->
+ <p v-if="items.length === 0"> 🥀No hay elementos en la lista🥀</p> 
 </template>
  
 <style scoped>
